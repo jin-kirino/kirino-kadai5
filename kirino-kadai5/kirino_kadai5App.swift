c100f48ad831d11a49f8237c3718aa4d531d8ba7
jin-kirino <jinkirino@gmail.com>
2023-03-09 07:40:28 +0900

    first push

---
+++ kirino-kadai5/kirino_kadai5App.swift
@@ -0,0 +1,17 @@
+//
+//  kirino_kadai5App.swift
+//  kirino-kadai5
+//
+//  Created by 神　樹里乃 on 2023/03/09.
+//
+
+import SwiftUI
+
+@main
+struct kirino_kadai5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
